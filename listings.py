2aa6bd3ac46cf0201257444e1582ab41c472fe0d
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-02-12 13:33:11 -0600

    Update listings.py

--- listings.py
+++ listings.py
@@ -8,12 +8,21 @@ livinListin = {'laptop':''}
 
 text = input('Wanna add anything to the list')
 
+'exit' = sys.exit() #ion think this work lmaooooo
+
 while text = 'Yes'.lower():
     print('Enter the name of the item')# i dont want to parse this so K.I.S.S
-    response = my_listing.append(input())#this this valid if not input(my_listings.append()) will b
-class app:#fuck idk how ima make this app I just need to holdmy shit oh I should type if I wanna append
+    thisgonegetOld = input()
+    if thisgonegetOld != 'exit()':#ion think this keep loopin       
+        response = my_listing.append_values(input())#this this valid if not input(my_listings.append()) will b
+        print('$$$PriceTag')
+        responseKeys = my_listing.append_keys(input())
 
-    def __init__(self):
-        pass #NEVA know what to put here
+#im finna go get my shoppin list right
 
-    def
+#class app:#fuck idk how ima make this app I just need to holdmy shit oh I should type if I wanna append
+
+#    def __init__(self):
+#        pass #NEVA know what to put here
+
+#    def
