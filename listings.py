3b597d29c3053a1f24c5d08745bbd349a715268c
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-02-12 11:28:12 -0600

    Create listings.py

---
+++ listings.py
@@ -0,0 +1,19 @@
+#this gone be a Item list with prices
+#BRB
+import pprint
+
+my_listing = {'Batana Oil':'like 20 bucks','':''}
+
+livinListin = {'laptop':''}
+
+text = input('Wanna add anything to the list')
+
+while text = 'Yes'.lower():
+    print('Enter the name of the item')# i dont want to parse this so K.I.S.S
+    response = my_listing.append(input())#this this valid if not input(my_listings.append()) will b
+class app:#fuck idk how ima make this app I just need to holdmy shit oh I should type if I wanna append
+
+    def __init__(self):
+        pass #NEVA know what to put here
+
+    def
