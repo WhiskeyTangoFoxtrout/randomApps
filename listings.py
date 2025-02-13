85d57e236cc5086bb9ce3c6d85889d9d4a03b53a
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-02-13 16:37:51 -0600

    Update listings.py

--- listings.py
+++ listings.py
@@ -6,23 +6,34 @@ my_listing = {'Batana Oil':'like 20 bucks','':''}
 
 livinListin = {'laptop':''}
 
-text = input('Wanna add anything to the list')
 
-'exit' = sys.exit() #ion think this work lmaooooo
-
-while text = 'Yes'.lower():
-    print('Enter the name of the item')# i dont want to parse this so K.I.S.S
-    thisgonegetOld = input()
-    if thisgonegetOld != 'exit()':#ion think this keep loopin       
-        response = my_listing.append_values(input())#this this valid if not input(my_listings.append()) will b
-        print('$$$PriceTag')
-        responseKeys = my_listing.append_keys(input())
+#ok so main function
 
 #im finna go get my shoppin list right
+#This my obj.method()/func()  app.name
+class app:#fuck idk how ima make this app I just need to holdmy shit oh I should type if I wanna append
 
-#class app:#fuck idk how ima make this app I just need to holdmy shit oh I should type if I wanna append
+    def __init__(self,name):#this supposed to take args. so yea the name of the app sounds goodshoudl makeit usrput
+        self.name = name #mak dis usrput
 
-#    def __init__(self):
-#        pass #NEVA know what to put here
+    def append(self,userAddition,maffin):#have a way to make this callable
 
-#    def
+        self.userAdditon = []
+        self.mafffin = maffin 
+        maffin = userAdditon#bite me aint no pointers LMAOO
+        #bite me
+        
+        while text <= 64:
+            if text +=1:
+                text = input('Wanna add anything to the list')
+                while text = 'Yes'.lower():
+                    print('Enter the name of the item')# i dont want to parse this so K.I.S.S
+                    thisgonegetOld = input()
+                    if thisgonegetOld != 'exit()':#ion think this keep loopin       
+                    response = my_listing.append_values(input())#this this valid if not input(my_listings.append()) will b
+                    print('$$$PriceTag')
+                    responseKeys = my_listing.append_keys(input())
+                else:
+                    continue #acutual go to the other function in the obj
+
+                
