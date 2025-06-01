23bd07fe5b00be5b861dd5b7086f1e49daa5f70a
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-06-01 15:48:21 -0500

    Update CharCpyIO.c

--- CharCpyIO.c
+++ CharCpyIO.c
@@ -4,7 +4,7 @@ simple way of cpyn data(char)*/
 #include <stdio.h>
 
 main(){
-
+//you have to int bc EOF might be a larger byte than char so you have to typecast int bc it  biggers
     int c;//type cast
 
     c = getchar();//1st instruction
@@ -14,5 +14,11 @@ main(){
         c  = getchar();//while in the program you can keep displaying your shit
         
     }
-  
+
+    /*
+    int d;
+    while((c = getchar()) !=EOF)
+        putchar(c);
+    */    
+    
 }
