357a68a807bad2f7f99c9b12df997bdbdd9224a3
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-06-01 15:32:37 -0500

    Create CharCpyIO.c

---
+++ CharCpyIO.c
@@ -0,0 +1,18 @@
+/*Book work ifykyk
+simple way of cpyn data(char)*/
+
+#include <stdio.h>
+
+main(){
+
+    int c;//type cast
+
+    c = getchar();//1st instruction
+    while (c != EOF){//conditon
+
+        putchar(c);//display instruction
+        c  = getchar();//while in the program you can keep displaying your shit
+        
+    }
+  
+}
