293f7966ce1be543375e52be15912a9068f3cb15
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-08-15 10:50:55 -0500

    Create cmdpmt.c

---
+++ cmdpmt.c
@@ -0,0 +1,12 @@
+//This is an app for something idk thinking os?
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+    printf("Options:/n
+            |(a)Files|   |(b)settings|   |(c) Exit|") //I have no idea how this gone work with out my own text or notepad app... ohk
+  
+}
